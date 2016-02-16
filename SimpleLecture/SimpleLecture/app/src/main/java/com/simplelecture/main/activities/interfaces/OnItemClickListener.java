05dd006fe9099b165f2bda6f6185892fd1e2eb38
GovindKarthik25 <gkprojects25@gmail.com>
2016-02-16 21:26:59 +0530

    Added listener for recycle view.

---
+++ SimpleLecture/SimpleLecture/app/src/main/java/com/simplelecture/main/activities/interfaces/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.simplelecture.main.activities.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by M1032185 on 2/16/2016.
+ */
+public interface OnItemClickListener {
+    public void onItemClick(View view,int position);
+}
