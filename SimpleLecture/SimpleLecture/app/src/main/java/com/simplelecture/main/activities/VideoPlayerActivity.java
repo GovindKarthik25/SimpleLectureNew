53967724ff907837b24e633d50d54f419de0c810
GovindKarthik25 <gkprojects25@gmail.com>
2016-02-23 19:24:47 +0530

    update

--- SimpleLecture/SimpleLecture/app/src/main/java/com/simplelecture/main/activities/VideoPlayerActivity.java
+++ SimpleLecture/SimpleLecture/app/src/main/java/com/simplelecture/main/activities/VideoPlayerActivity.java
@@ -13,20 +13,22 @@ import android.widget.VideoView;
 import com.simplelecture.main.R;
 import com.simplelecture.main.fragments.DashboardFragment;
 import com.simplelecture.main.http.ApiService;
+import com.simplelecture.main.http.NetworkLayer;
 import com.simplelecture.main.util.ConnectionDetector;
 import com.simplelecture.main.util.SnackBarManagement;
 import com.simplelecture.main.util.Util;
 
-public class VideoPlayerActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class VideoPlayerActivity extends AppCompatActivity implements NetworkLayer {
 
     private int position = 0;
-    private ProgressDialog progressDialog;
     VideoView videoView;
     // Insert your Video URL
-   // String VideoURL = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
-   // String VideoURL ="https://player.vimeo.com/video/49462103";
+    // String VideoURL = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+    // String VideoURL ="https://player.vimeo.com/video/49462103";
     //https://player.vimeo.com/video/124017989/config
-    String VideoURL = "https://skyfiregcs-a.akamaihd.net/exp=1456228769~acl=%2A%2F417392989.mp4%2A~hmac=72b702c5c083e14535952aa82d25ffd2f4b5634242451fdba3485a58159e851c/2tierchgci/vimeo-prod-skyfire-std-us/01/4803/4/124017989/417392989.mp4";
+    String videoURL = "https://skyfiregcs-a.akamaihd.net/exp=1456228769~acl=%2A%2F417392989.mp4%2A~hmac=72b702c5c083e14535952aa82d25ffd2f4b5634242451fdba3485a58159e851c/2tierchgci/vimeo-prod-skyfire-std-us/01/4803/4/124017989/417392989.mp4";
     private String videoId;
     private ProgressDialog pd;
     private boolean param_get_VideoPlayer = false;
@@ -47,6 +49,22 @@ public class VideoPlayerActivity extends AppCompatActivity {
         //we use onRestoreInstanceState in order to play the video playback from the stored position
         position = savedInstanceState.getInt("Position");
         videoView.seekTo(position);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(videoView.isPlaying()){
+            videoView.pause();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 
     @Override
@@ -72,41 +90,15 @@ public class VideoPlayerActivity extends AppCompatActivity {
         } else {
             snack.snackBarNotification(coordinatorLayout, 1, getResources().getString(R.string.noInternetConnection), getResources().getString(R.string.dismiss));
         }
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
 
-        // create a progress bar while the video file is loading
-        progressDialog = new ProgressDialog(VideoPlayerActivity.this);
-        // set a message for the progress bar
-        progressDialog.setMessage("Loading...");
-        //set the progress bar not cancelable on users' touch
-        progressDialog.setCancelable(false);
-        // show the progress bar
-        progressDialog.show();
 
-        try {
-
-            Uri video = Uri.parse(VideoURL);
-            //set the uri of the video to be played
-            videoView.setVideoURI(video);
-
-            //set the media controller in the VideoView
-            videoView.setMediaController(mediaController);
-
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
-
-        videoView.requestFocus();
         //we also set an setOnPreparedListener in order to know when the video file is ready for playback
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
 
             public void onPrepared(MediaPlayer mediaPlayer) {
                 Log.i("TAG", "Duration = " + videoView.getDuration());
                 // close the progress bar and play the video
-                progressDialog.dismiss();
+                pd.dismiss();
                 //if we have a position on savedInstanceState, the video playback should start from here
                 videoView.seekTo(position);
                 if (position == 0) {
@@ -120,13 +112,81 @@ public class VideoPlayerActivity extends AppCompatActivity {
 
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
-        public void onCompletion(MediaPlayer mp) {
-            if (progressDialog.isShowing()) {
-                progressDialog.dismiss();
+            public void onCompletion(MediaPlayer mp) {
+                if (pd.isShowing()) {
+                    pd.dismiss();
+                }
+                finish();
             }
-            finish();
-        }
-    });
+        });
 
+    }
+
+    private void OnPrepareVideoPlay(String videoURLID) {
+        try {
+
+
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
+            videoView.setMediaController(mediaController);
+
+            pd = new Util().waitingMessage(VideoPlayerActivity.this, "", getResources().getString(R.string.loading));
+            pd.setCanceledOnTouchOutside(false);
+
+            Uri video = Uri.parse(videoURLID);
+            //set the uri of the video to be played
+            videoView.setVideoURI(video);
+
+            //set the media controller in the VideoView
+            videoView.setMediaController(mediaController);
+
+            videoView.requestFocus();
+
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void parseResponse(String response) {
+        try {
+            if (pd.isShowing()) {
+                pd.dismiss();
+            }
+
+            JSONObject jSONObject = new JSONObject(response);
+            String getUrlprogressiveContent = jSONObject.getString("progressive");
+            JSONObject object2 = new JSONObject(getUrlprogressiveContent);
+            videoURL = object2.getString("url");
+
+            OnPrepareVideoPlay(videoURL);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void showError(String error) {
+        try {
+            if (pd.isShowing()) {
+                pd.dismiss();
+            }
+
+            if(error.isEmpty()){
+                error = "Error in Video";
+            }
+
+            snack.snackBarNotification(coordinatorLayout, 1, error, getResources().getString(R.string.dismiss));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
